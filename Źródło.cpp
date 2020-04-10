bfcaef5809a5fbedc421762f8a501404831c0300
Andrzej Wasilewski <andrzej.wasilewski80@gmail.com>
2020-04-10 10:59:02 +0200

    Update Źródło.cpp

---
+++ Źródło.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int main(int argc, char * arv[])
+{
+	cout << endl;
+
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
+
+
+
+
